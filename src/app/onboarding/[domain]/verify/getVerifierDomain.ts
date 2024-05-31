c41f1b663c7dc66c04ea8bcddb4d3c4300cb7dca
CIAMShrek <158512442+ciamshrek@users.noreply.github.com>
2024-05-31 10:30:23 -0700

    v1 hide the a0 export

--- src/app/onboarding/[domain]/verify/getVerifierDomain.ts
+++ src/app/onboarding/[domain]/verify/getVerifierDomain.ts
@@ -1,5 +1,11 @@
 import { sha256 } from "./sha256";
 
+/**
+ * 
+ * @param domain 
+ * @param userId 
+ * @returns 
+ */
 export function getVerifierDomain(domain: string, userId: string) {
-    return `a101${sha256(userId)}.${domain}`;
+    return `${sha256(userId)}.${domain}`;
 }
